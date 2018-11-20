8dc2b996d3dd2efbb190be5505b19a2d2d166e9b
Ballabh Heera <44017701+heerajoshi@users.noreply.github.com>
2018-11-20 12:28:20 +0530

    Add some new tests for map filter and reduce

--- arrayFunctionsTest.js
+++ arrayFunctionsTest.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const {map,filter,reduce} = require('./arrayFunctions.js');
 
+//=====Map
 const add5 = function(element) {
   return element + 5;
 }
@@ -9,7 +10,13 @@ const square = function(element){
   return element * element;
 }
 
-let inputArr = [2, 3, 4];
+let inputArr = [];
+assert.deepEqual(map(add5, inputArr), [] );
+
+inputArr = [9];
+assert.deepEqual(map(add5, inputArr), [14] );
+
+inputArr = [2, 3, 4];
 assert.deepEqual(map(add5, inputArr), [7, 8, 9] );
 
 inputArr = [1, 0, 6, 9, 100];
@@ -21,9 +28,21 @@ assert.deepEqual(map(square, inputArr), [4, 9, 16] );
 inputArr = [1, 0, 6, 9, 100];
 assert.deepEqual(map(square, inputArr), [1, 0, 36, 81, 10000] );
 
+//=========Filter
+
 const isEven = function(element){
-  return element % 2 === 0
+  return element % 2 === 0;
 }
+
+const isOdd = function(element){
+  return element % 2 !== 0;
+}
+
+inputArr = [];
+assert.deepEqual(filter(isOdd, inputArr), [] );
+
+inputArr = [9];
+assert.deepEqual(filter(isOdd, inputArr), [9] );
 
 inputArr = [1, 0, 6, 9, 100];
 assert.deepEqual(filter(isEven, inputArr), [0, 6, 100] );
@@ -31,13 +50,37 @@ assert.deepEqual(filter(isEven, inputArr), [0, 6, 100] );
 inputArr = [2, 3, 4, 8];
 assert.deepEqual(filter(isEven, inputArr), [2, 4, 8] );
 
+inputArr = [1, 0, 6, 9, 100];
+assert.deepEqual(filter(isOdd, inputArr), [1,9] );
+
+inputArr = [2, 3, 4, 8, 9, 0, 1];
+assert.deepEqual(filter(isOdd, inputArr), [3, 9, 1] );
+
+//=========Reduce
+
 const sum = function(a,b){
   return a+b;
 }
+
+const mul = function(a,b){
+  return a*b;
+}
+
+//inputArr = [6];
+//assert.deepEqual(reduce(sum, inputArr), 6 );
+
+inputArr = [9];
+assert.deepEqual(reduce(mul, inputArr, 8), 72 );
 
 inputArr = [2, 3, 4, 8];
 assert.deepEqual(reduce(sum, inputArr), 17 );
 
 inputArr = [2, 3, 4, 8];
 assert.deepEqual(reduce(sum, inputArr, 10), 27 );
+
+inputArr = [2, 3, 4, 8];
+assert.deepEqual(reduce(mul, inputArr), 192 );
+
+inputArr = [2, 3, 4, 8];
+assert.deepEqual(reduce(mul, inputArr, 10), 1920 );
 
